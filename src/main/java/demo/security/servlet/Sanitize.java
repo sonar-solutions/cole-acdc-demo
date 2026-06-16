2a2ed6592123de338c928896b92aa81913b503a5
Cole Gannaway <cole.gannaway@sonarsource.com>
2026-06-15 20:51:42 -0500

    created a second endpoint

---
+++ src/main/java/demo/security/servlet/Sanitize.java
@@ -0,0 +1,8 @@
+package demo.security.servlet;
+
+public class Sanitize {
+    // This method sanitizes SQL input by removing potentially dangerous characters such as single quotes, double quotes, and semicolons.
+    public static String sanitizeSQL(String input) {
+        return input;
+    }
+}
